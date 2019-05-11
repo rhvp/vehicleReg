320dbc2ff8feb421edd8e1a5c5d479f431db711b
sody <bugsabdul@yahoo.com>
2019-05-11 12:58:38 +0100

    fifth commit

--- php_scripts/signup.php
+++ php_scripts/signup.php
@@ -12,10 +12,12 @@ if(isset($_POST['submit'])){
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     
-    $query = "INSERT INTO users(username,password,firstname,lastname,phone,address) VALUES ('$username','$password','$firstname','$lastname','$phone','$address')";
+    $query = "INSERT INTO users (username,password,firstname,lastname,phone,address) VALUES ('$username','$password','$firstname','$lastname','$phone','$address')";
     
-    if(mysqli_query($conn,$query)){
+    if(mysqli_query($conn, $query)) {
         header("location: ../dashboard.php?login=yes");
+    } else {
+        header("location: ../index.html?signup=failed");
     }
     
 }
